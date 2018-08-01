6f0d3be65d0e1fafaf50d7a03331eaea4d3edef4
Gavin <Gavin@Gavin-Work>
2018-08-01 18:49:24 +0800

    test

--- saga/saga-common/src/main/java/org/saga/javacode/Generic.java
+++ saga/saga-common/src/main/java/org/saga/javacode/Generic.java
@@ -6,7 +6,7 @@ public class Generic {
 	public static void main(String[] args) {
 //		List<Integer> li = new ArrayList<Integer>();
 //		List<Number> ln = li;
-		
+		System.out.println("test");
 	}
 
 }
